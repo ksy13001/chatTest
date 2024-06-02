da104ca425985576ff1119f9868a45369b9f0220
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-06-02 23:26:34 +0900

    메세지 바로 전송하게 수정

--- src/main/java/com/example/chatTest/controller/MessageController.java
+++ src/main/java/com/example/chatTest/controller/MessageController.java
@@ -19,12 +19,17 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import static org.hibernate.query.sqm.tree.SqmNode.log;
+
+/**
+ *  메세지 관련 컨르롤러
+ * */
 
 @Slf4j
 @RequiredArgsConstructor
@@ -35,36 +40,17 @@ public class MessageController {
     private final ChatRoomService chatRoomService;
     private final MessageService messageService;
     private final MessageSendingOperations messageSendingOperations;
+    private final SimpMessageSendingOperations template;
 
-    Logger logger = LoggerFactory.getLogger(MessageController.class);
-
-    @MessageMapping("/api/v1/chat/{chatRoomId}")
-    @SendTo("/sub/api/v1/chat/{chatRoomId}")
-    public MessageDto send(@DestinationVariable Long chatRoomId,
-                           @Payload MessageDto messageDto)
-    {      // 트렌젝션 범위내에서 실행, 서비스랑 컨트롤러 분리좀 하기
-//        User user = userService.findById(messageDto.getUserId());
-//        ChatRoom chatRoom = chatRoomService.findById(messageDto.getRoomId());
-//        logger.info("messageDto: " + messageDto.getUserId() + " " + messageDto.getRoomId());
-//        logger.info("chatRoom: " + chatRoom.getId() + " " + chatRoom.getName() + " " + chatRoom.getTotalUser());
-//        logger.info("user: " + user.getId() +" "+ user.getName());
-//        Message message = messageService.setMessage(messageDto, user, chatRoom);
-//        UserChatRoom userChatRoom = message.getUserChatRoom();
-//        messageSendingOperations.convertAndSend(MessageDto.builder()
-//                .id(message.getId())
-//                .content(message.getContent())
-//                .roomId(userChatRoom.getChatRoom().getId())
-//                .userId(userChatRoom.getUser().getId())
-//                .build());
-        logger.info("메세지 저장");
-        Message message = messageService.sendMessage(messageDto.getUserId(), messageDto.getRoomId(), messageDto.getContent());
-
-
-        return MessageDto.builder()
-                .id(message.getId())
-                .content(message.getContent())
-                .roomId(message.getUserChatRoom().getChatRoom().getId())
-                .userId(message.getUserChatRoom().getUser().getId())
-                .build();
+    /**
+     * 메세지 전송
+     * */
+    @MessageMapping("/api/chat/{chatRoomId}")
+    public void send(@Payload MessageDto messageDto)
+    {
+        messageService.save(messageDto);
+        template.convertAndSend("/sub/api/chat/"+messageDto.getRoomId(), messageDto);
     }
+
+
 }
