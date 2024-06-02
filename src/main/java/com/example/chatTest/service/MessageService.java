a7d19dfc863a5e706cc98669945a4e7ff178f64a
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-06-02 23:24:25 +0900

    메세지 저장(수정필요)

--- src/main/java/com/example/chatTest/service/MessageService.java
+++ src/main/java/com/example/chatTest/service/MessageService.java
@@ -7,12 +7,15 @@ import com.example.chatTest.domain.UserChatRoom;
 import com.example.chatTest.dto.MessageDto;
 import com.example.chatTest.repository.ChatRoomRepository;
 import com.example.chatTest.repository.MessageRepository;
+import com.example.chatTest.repository.UserChatRoomRepository;
 import com.example.chatTest.repository.UserRepository;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Slf4j
@@ -24,30 +27,42 @@ public class MessageService {
     private final MessageRepository messageRepository;
     private final UserRepository userRepository;
     private final ChatRoomRepository chatRoomRepository;
-    public Long save(Message message){
-        return messageRepository.save(message).getId();
+    private final UserChatRoomRepository userChatRoomRepository;
+
+    public Long save(MessageDto messageDto){
+
+        UserChatRoom userChatRoom = userChatRoomRepository
+                .findByUserIdAndChatRoomId(messageDto.getUserId(), messageDto.getRoomId());
+        return messageRepository.save(messageDto.toEntity(userChatRoom)).getId();
     }
 
-    public Message sendMessage(Long userId, Long chatRoomId, String content){
-        User user = userRepository.findById(userId).orElseThrow(()->new IllegalArgumentException("유저가 존재하지 않습니다. id="
-                + userId));
-        ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow(()->new IllegalArgumentException("채팅방이 존재하지 않습니다. id="
-                + chatRoomId));
-        UserChatRoom userChatRoom = UserChatRoom.setUserChatRoom(user, chatRoom);
-        Message message = Message.builder()
-                .content(content)
-                .userChatRoom(userChatRoom)
-                .build();
+    /**
+     * 채팅방 채팅 조회
+     * */
+//    public List<Message> findAll(Long chatRoomId){
+//
+//    }
 
-        return messageRepository.save(message);
-    }
-
-    public Message setMessage(MessageDto messageDto, User user, ChatRoom chatRoom){
-        UserChatRoom userChatRoom = UserChatRoom.setUserChatRoom(user, chatRoom);
-        return Message.builder()
-                .content(messageDto.getContent())
-                .userChatRoom(userChatRoom)
-                .build();
-    }
+//    public Message sendMessage(Long userId, Long chatRoomId, String content){
+//        User user = userRepository.findById(userId).orElseThrow(()->new IllegalArgumentException("유저가 존재하지 않습니다. id="
+//                + userId));
+//        ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow(()->new IllegalArgumentException("채팅방이 존재하지 않습니다. id="
+//                + chatRoomId));
+//        UserChatRoom userChatRoom = UserChatRoom.setUserChatRoom(user, chatRoom);
+//        Message message = Message.builder()
+//                .content(content)
+//                .userChatRoom(userChatRoom)
+//                .build();
+//
+//        return messageRepository.save(message);
+//    }
+//
+//    public Message setMessage(MessageDto messageDto, User user, ChatRoom chatRoom){
+//        UserChatRoom userChatRoom = UserChatRoom.setUserChatRoom(user, chatRoom);
+//        return Message.builder()
+//                .content(messageDto.getContent())
+//                .userChatRoom(userChatRoom)
+//                .build();
+//    }
 
 }
