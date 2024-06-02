54d8f46591db5b1e903dfba34d77c3be7d696063
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-06-02 23:21:45 +0900

    findAllDesc 추가

--- src/main/java/com/example/chatTest/repository/ChatRoomRepository.java
+++ src/main/java/com/example/chatTest/repository/ChatRoomRepository.java
@@ -2,9 +2,13 @@ package com.example.chatTest.repository;
 
 import com.example.chatTest.domain.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ChatRoomRepository  extends JpaRepository<ChatRoom, Long> {
-    ChatRoom findByName(String name);
+    @Query("Select c FROM ChatRoom c ORDER BY c.id DESC ")
+    List<ChatRoom> findAllDesc();
 }
