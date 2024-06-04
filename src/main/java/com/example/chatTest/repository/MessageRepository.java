1d09c6c7086bba1dc258ece25b9ce0ebd17ea9cd
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-06-04 23:26:41 +0900

    findAllByChatRoomId 추가

--- src/main/java/com/example/chatTest/repository/MessageRepository.java
+++ src/main/java/com/example/chatTest/repository/MessageRepository.java
@@ -1,8 +1,14 @@
 package com.example.chatTest.repository;
 
 import com.example.chatTest.domain.Message;
+import com.example.chatTest.service.MessageService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+    List<Message> findAllByChatRoomId(Long chatRoomId);
 
+    //List<Message> findAllByChatRoomIdOrderById(Long chatRoomId);
 }
