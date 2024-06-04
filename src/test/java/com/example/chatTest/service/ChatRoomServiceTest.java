c7ead881d8058312b347451cd2d7ed1cc7aa9ba2
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-06-04 23:23:21 +0900

    채팅장 입장, 퇴장 테스트 추가

--- src/test/java/com/example/chatTest/service/ChatRoomServiceTest.java
+++ src/test/java/com/example/chatTest/service/ChatRoomServiceTest.java
@@ -1,8 +1,10 @@
 package com.example.chatTest.service;
 
 import com.example.chatTest.domain.ChatRoom;
+import com.example.chatTest.domain.ChatRoomType;
 import com.example.chatTest.domain.User;
 import com.example.chatTest.domain.UserChatRoom;
+import com.example.chatTest.dto.CreateChatRoomRequestDto;
 import com.example.chatTest.repository.ChatRoomRepository;
 import com.example.chatTest.repository.UserChatRoomRepository;
 import jakarta.transaction.Transactional;
@@ -17,7 +19,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.registerCustomDateFor
 import static org.junit.jupiter.api.Assertions.*;
 
 @Transactional
-@Rollback(value = false)
 @SpringBootTest
 class ChatRoomServiceTest {
 
@@ -31,31 +32,31 @@ class ChatRoomServiceTest {
         //give
         String chatRoomName = "room1";
         String userName = "ksy";
-        ChatRoom chatRoom = ChatRoom.builder()
-                .name(chatRoomName)
-                .totalUser(1)
-                .build();
+
+        // 유저
         User user = User.builder()
                 .name(userName)
                 .build();
         userService.join(user);
 
-        UserChatRoom.setUserChatRoom(user, chatRoom);
+        CreateChatRoomRequestDto request = CreateChatRoomRequestDto.builder()
+                .name(chatRoomName)
+                .userId(user.getId())
+                .typeId(ChatRoomType.SONG, 1L)
+                .build();
         //when
-        chatRoomService.create(chatRoom, user.getId());
+        Long chatRoomId = chatRoomService.create(request);
+        ChatRoom chatRoom = chatRoomService.findById(chatRoomId);
         //then
         assertThat(chatRoom.getName()).isEqualTo(chatRoomName);
-        assertThat(chatRoom.getTotalUser()).isEqualTo(1);
+        assertThat(chatRoom.getTotalMember()).isEqualTo(1);
     }
 
     @Test
     public void 채팅방_입장() throws Exception{
+        // 3명이 1번방에 입장. 1번방은 start가 생성
         //given
-        // 방생성
-        ChatRoom chatRoom = ChatRoom.builder()
-                .name("room1")
-                .totalUser(1)
-                .build();
+
         // 유저생성
         User start = User.builder().name("start").build();
         User user1 = User.builder().name("user1").build();
@@ -66,40 +67,71 @@ class ChatRoomServiceTest {
         Long id1 = userService.join(user1);
         Long id2 = userService.join(user2);
         Long id3 = userService.join(user3);
+
+        // 채팅방 dto 생성
+        String chatRoomName = "room1";
+
+        CreateChatRoomRequestDto request = CreateChatRoomRequestDto.builder()
+                .name(chatRoomName)
+                .userId(id0)
+                .typeId(ChatRoomType.SONG, 1L)
+                .build();
+
         //when
         // 채팅방 생성
-        chatRoomService.create(chatRoom, id0);
+        Long chatRoomId = chatRoomService.create(request);
+        ChatRoom chatRoom = chatRoomService.findById(chatRoomId);
         // 유저 입장
-        chatRoomService.enter(chatRoom.getId(), id1);
-        chatRoomService.enter(chatRoom.getId(), id2);
-        chatRoomService.enter(chatRoom.getId(), id3);
+        chatRoomService.enterChatRoom(id1, chatRoomId);
+        chatRoomService.enterChatRoom(id2, chatRoomId);
+        chatRoomService.enterChatRoom(id3, chatRoomId);
         //then
-        assertThat(chatRoom.getTotalUser()).isEqualTo(3);
+        assertThat(chatRoom.getCurrentMember()).isEqualTo(4);
+        // 제일 첫번째 사람
+        assertThat(chatRoom.getMasterId()).isEqualTo(id0);
     }
-
+//
     @Test
     public void 채팅방_퇴장() throws Exception{
-        // 3명에서 2명 나감, 방에 사람 없을떄 방 없어지는거 구현해야함
+        // 4명에서 2명 나감, 방에 사람 없을떄 방 없어지는거 구현해야함
         //given
-        // 방생성
-        ChatRoom chatRoom = ChatRoom.builder()
-                .name("room1")
-                .totalUser(1)
-                .build();
+        // 유저생성
+        User start = User.builder().name("start").build();
         User user1 = User.builder().name("user1").build();
         User user2 = User.builder().name("user2").build();
         User user3 = User.builder().name("user3").build();
+        // 유저 가입
+        Long id0 = userService.join(start);
         Long id1 = userService.join(user1);
         Long id2 = userService.join(user2);
         Long id3 = userService.join(user3);
 
-        chatRoomService.create(chatRoom, id1);
-        chatRoomService.enter(chatRoom.getId(), id2);
-        chatRoomService.enter(chatRoom.getId(), id3);
+        // 채팅방 dto 생성
+        String chatRoomName = "room1";
+
+        CreateChatRoomRequestDto request = CreateChatRoomRequestDto.builder()
+                .name(chatRoomName)
+                .userId(id0)
+                .typeId(ChatRoomType.SONG, 1L)
+                .build();
+
+        // 채팅방 생성
+        Long chatRoomId = chatRoomService.create(request);
+        ChatRoom chatRoom = chatRoomService.findById(chatRoomId);
+
+        // 유저 입장
+        chatRoomService.enterChatRoom(id1, chatRoomId);
+        chatRoomService.enterChatRoom(id2, chatRoomId);
+        chatRoomService.enterChatRoom(id3, chatRoomId);
+
         //when
-        chatRoomService.leave(chatRoom.getId(), id1);
-        chatRoomService.leave(chatRoom.getId(), id2);
+        //유저 퇴장
+        chatRoomService.leaveChatRoom(id0, chatRoomId);
+        chatRoomService.leaveChatRoom(id1, chatRoomId);
         //then
-        assertThat(chatRoom.getTotalUser()).isEqualTo(1);
-    }
+        // 4-2
+        assertThat(chatRoom.getCurrentMember()).isEqualTo(2);
+        // 현재 방에서 가장 오래된 사람은 id2
+        assertThat(chatRoom.getMasterId()).isEqualTo(id2);
+  }
 }
