3e354c5054771c3bd8afe56da33d04bd211de760
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-06-04 23:34:24 +0900

    chatRoomType 추가

--- src/main/java/com/example/chatTest/domain/ChatRoom.java
+++ src/main/java/com/example/chatTest/domain/ChatRoom.java
@@ -2,6 +2,7 @@ package com.example.chatTest.domain;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Null;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -35,33 +36,45 @@ public class ChatRoom extends BaseTimeEntity {
     // 현재 방에 있는 사람 수
     private int currentMember;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="song_id")
-    private Song song;
+    @Enumerated(EnumType.STRING)
+    private ChatRoomType type;
+
 
     @Builder
-    public ChatRoom(String name) {
+    public ChatRoom(String name, ChatRoomType chatRoomType) {
         this.name = name;
+        this.type = chatRoomType;
         this.currentMember = 1; // 생성될때 멤버 1명
     }
     /**
      * 비즈니스 메서드
      **/
+    // 수정 필요
     public int getTotalMember(){
         return this.getUserChatRooms().size();
     }
 
-//    // 방에 사람 들어오고
-//    public void addUser() {
-//        this.totalUser += 1;
-//    }
-//
-//    // 나가고
-//    public void subUser() {
-//        if (totalUser > 0) {
-//            this.totalUser -= 1;
-//        }
-//    }
+    // 채팅방의 제일 먼저 들어온 유저 아이디 가져가기(아무거나 가져가도 상관은없음. 일단은 제일앞에거)
+    public Long getMasterId(){
+        List<UserChatRoom> userChatRoom = this.getUserChatRooms();
+        if (userChatRoom.isEmpty()){
+            throw new NullPointerException();
+        }
+        else{
+            return userChatRoom.get(0).getUser().getId();
+        }
+    }
+
+    // 채팅방에 유저 입장
+    public void addUser(){
+        this.currentMember += 1;
+    }
+
+    // 채팅방 유저 퇴장
+    public void subUser(){
+        this.currentMember -= 1;
+    }
+
 }
 
 
