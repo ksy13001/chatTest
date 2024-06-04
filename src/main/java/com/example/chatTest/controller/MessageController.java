aa686b72f6b9e6168b6264f736c8c76d4b8d5e9c
DESKTOP-1ROFC7Q\ksy13 <ksy13001@gmail.com>
2024-06-04 23:31:51 +0900

    send: 메세지 타입별로 추가 수정 필요

--- src/main/java/com/example/chatTest/controller/MessageController.java
+++ src/main/java/com/example/chatTest/controller/MessageController.java
@@ -1,13 +1,13 @@
 package com.example.chatTest.controller;
 
-import com.example.chatTest.domain.ChatRoom;
-import com.example.chatTest.domain.Message;
-import com.example.chatTest.domain.User;
-import com.example.chatTest.domain.UserChatRoom;
+import com.example.chatTest.domain.*;
 import com.example.chatTest.dto.MessageDto;
+import com.example.chatTest.dto.MessageListDto;
 import com.example.chatTest.service.ChatRoomService;
 import com.example.chatTest.service.MessageService;
 import com.example.chatTest.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.message.SimpleMessage;
@@ -21,9 +21,9 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static org.hibernate.query.sqm.tree.SqmNode.log;
 
@@ -39,18 +39,36 @@ public class MessageController {
     private final UserService userService;
     private final ChatRoomService chatRoomService;
     private final MessageService messageService;
-    private final MessageSendingOperations messageSendingOperations;
     private final SimpMessageSendingOperations template;
 
     /**
      * 메세지 전송
+     * 메세지타입 : CHAT, JOIN, LEAVE, PLAYER
      * */
     @MessageMapping("/api/chat/{chatRoomId}")
     public void send(@Payload MessageDto messageDto)
     {
-        messageService.save(messageDto);
-        template.convertAndSend("/sub/api/chat/"+messageDto.getRoomId(), messageDto);
+        if(messageDto.getType() == MessageType.JOIN){
+
+        }
+        if(messageDto.getType() == MessageType.CHAT) {
+            messageService.save(messageDto);
+            template.convertAndSend("/sub/api/chat/" + messageDto.getChatRoomId(), messageDto);
+        }
     }
 
+    /**
+     * 채팅방의 모든 채팅 조회
+     * */
+    @GetMapping("/api/chat/{chatRoomId}")
+    public Result messages(@PathVariable(name = "chatRoomId")Long chatRoomId ){
+        return new Result(messageService.findAllDesc(chatRoomId));
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private T data;
+    }
 
 }
